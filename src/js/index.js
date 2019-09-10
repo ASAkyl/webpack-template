170858a63e0d56ef6a1f968ab2054a6afa855265
Akylbek Mansurov <akylbek.mansurov@protonmail.com>
2019-09-10 15:04:56 +0600

    init

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap'
+import '../scss/style.scss'
+
+import $ from 'jquery'
+
+$(function () {
+    // $('body').addClass('text-center')
+
+    $('#test-id').text('added by jQuery')
+})
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: [
+        './src/js/index.js',
+        './src/scss/style.scss',
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public', 'assets')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/,
+                    /vendor/,
+                ],
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: MiniCssExtractPlugin.loader
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                        plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ]
+};
