6c10b77f997a97196c315d81a7aab492e46e5e5c
Akylbek Mansurov <mansurov.akylbek@protonmail.com>
2019-11-18 18:08:15 +0600

    added ESLint with airbnb config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 module.exports = {
-    entry: [
-        './src/js/index.js',
-        './src/scss/style.scss',
+  entry: [
+    './src/js/index.jsx',
+    './src/scss/style.scss',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'assets'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [
+          /node_modules/,
+          /vendor/,
+        ],
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: MiniCssExtractPlugin.loader,
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins() { // postcss plugins, can be exported to postcss.config.js
+              return [
+                autoprefixer,
+              ];
+            },
+          },
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public', 'assets')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [
-                    /node_modules/,
-                    /vendor/,
-                ],
-                use: {
-                    loader: "babel-loader"
-                },
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: [{
-                    loader: 'style-loader', // inject CSS to page
-                }, {
-                    loader: MiniCssExtractPlugin.loader
-                }, {
-                    loader: 'css-loader', // translates CSS into CommonJS modules
-                }, {
-                    loader: 'postcss-loader', // Run postcss actions
-                    options: {
-                        plugins: function () { // postcss plugins, can be exported to postcss.config.js
-                            return [
-                                require('autoprefixer')
-                            ];
-                        }
-                    }
-                }, {
-                    loader: 'sass-loader' // compiles Sass to CSS
-                }]
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
 };
