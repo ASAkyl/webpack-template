ec4e9cae308a01f1d698d3a7900a494b62aae3e7
Akylbek Mansurov <mansurov.akylbek@protonmail.com>
2019-11-25 17:42:40 +0600

    changed ESLint config form airbnb to StandardJS

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  constructor (props) {
+    super(props)
+    this.state = {}
   }
 
-  render() {
-    const message = 'Hello World';
+  render () {
+    const message = 'Hello World'
 
     return (
       <>
         {message}
       </>
-    );
+    )
   }
 }
 
-export default App;
+export default App
